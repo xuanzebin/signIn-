d1775b143ae801b4d5026d39860c345182adc5a4
xuanzebin <xuanzebin@126.com>
2018-12-19 23:25:45 +0800

    添加了签到页面按钮的状态切换

--- pages/create-signform/create-signform.js
+++ pages/create-signform/create-signform.js
@@ -7,17 +7,22 @@ Page({
   },
   signformSubmit:function(e){
     let newForm=e.detail.value
-    var FormList = AV.Object.extend('formList');
+    var FormList = AV.Object.extend('formList')
     // 新建对象
-    var formList = new FormList();
+    var formList = new FormList()
     // 设置名称
-    formList.set('name', newForm.name);
-    formList.set('creater', newForm.creater);
+    formList.set('name', newForm.name)
+    formList.set('creater', newForm.creater)
     formList.set('time', newForm.time);
-    formList.set('remarks', newForm.remarks);
+    formList.set('remarks', newForm.remarks)
+    formList.set('signIn',false)
+    formList.set('leave',false)
     // 设置优先级
     formList.save().then(function (formList) {
       console.log('objectId is ' + formList.id);
+      newForm.signIn=false
+      newForm.leave=false
+      newForm.id=formList.id
       app.data.formList.push(newForm)
       wx.navigateBack({
         delta: 1
--- pages/sign/sign.js
+++ pages/sign/sign.js
@@ -20,26 +20,45 @@ Page({
       .then(todos => {
         let array=[]
         todos.forEach((value,index)=>{
-          let {name,time,creater,remarks}=value.attributes
-          array.push({ name, time, creater, remarks})
+          let {name,time,creater,remarks,signIn,leave}=value.attributes
+          let id=value.id
+          array.push({name,time,creater,remarks,signIn,leave,id})
         })
-        console.log(array)
-        // this.setData({formList:array})
         app.data.formList=array
         this.setData({ formList: app.data.formList })
       })
       .catch(console.error);
   },
   onShow:function(e){
-    console.log('show')   
     if (this.data.formList !== app.data.formList){
       this.setData({formList:app.data.formList})
     }
-    console.log(this.data.formList)
   }, 
   createSign:function(e){
     wx.navigateTo({
       url: '../create-signform/create-signform',
     })
+  },
+  signInConfirm:function(e){
+    let formIndex=e.currentTarget.dataset.index
+    if (app.data.formList[formIndex].signIn === true) return
+    app.data.formList[formIndex].signIn = true
+    this.setData({ formList: app.data.formList })
+    let updateSignIn = AV.Object.createWithoutData('formList', this.data.formList[formIndex].id)
+    // 修改属性
+    updateSignIn.set('signIn', true)
+    // 保存到云端
+    updateSignIn.save()
+  },
+  leaveConfirm:function(e){
+    let formIndex = e.currentTarget.dataset.index
+    if (app.data.formList[formIndex].leave === true) return
+    app.data.formList[formIndex].leave = true
+    this.setData({ formList: app.data.formList })
+    let updateSignIn = AV.Object.createWithoutData('formList', this.data.formList[formIndex].id)
+    // 修改属性
+    updateSignIn.set('leave', true)
+    // 保存到云端
+    updateSignIn.save()
   }
 })
